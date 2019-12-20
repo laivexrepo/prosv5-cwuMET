96a7da44656ebec0d0034d078aa38cdac75c43c1
Willem Scholten <wscholten@seaprep.org>
2019-12-19 18:27:23 -0800

    Added claw autonomous code module to open/close claw

--- include/claw.h
+++ include/claw.h
@@ -1,6 +1,13 @@
 #ifndef CLAW_H_
 #define CLAW_H_
 
+extern float maxAngleClawOpen;
+extern int clawGearRatio;
+
 void clawControl();
+
+void clawSetLowPreset();
+
+float clawOpenForAngle(float angle, int speed );
 
 #endif
--- src/claw.cpp
+++ src/claw.cpp
@@ -3,6 +3,13 @@
 #include "portdef.h"
 #include "claw.h"
 
+float maxAngleClawOpen = 90;        // Maximum angle for the claw to move to
+                                    // when it is at it's most open position
+
+int clawGearRatio = 3;              // gear ratio between motor and claw arm gear
+                                    // if a 12gear drives a 36gear the ratio is 36/12 = 3
+                                    // for direct drive it is 1
+
 void clawControl(){
   // you could make clawCOntrol also take an input varibale for speed,
   // perhaps an speedOpen and a speedClose
@@ -15,4 +22,57 @@ void clawControl(){
    else {
      clawMotor.move_velocity(0);
    }
+}
+
+void clawSetLowPreset() {
+  // Set zero point of the claw, by doing a tare_position on the claw motors,
+  // from which point forward all movement can be calculated by number of
+  // degree of rotation upwards/downwards -- inwards/outwards for the claw.
+
+  // the tare_position is set when an assigned button on the controller is
+  // pressed, it writes to the LCD screen on the brain that it is awaiting
+  // the setting of a 0 point.
+
+  // This function should be called in the void competition_initialize() function of
+  // main.cpp
+
+  clawMotor.tare_position();
+  if(DEBUG) { std::cout << "Claw Encoder: " << clawMotor.get_position() << " \n"; }
+
+  pros::lcd::print(2, "ZERO point for claw set");
+  pros::delay(20);
+}
+
+float clawOpenForAngle(float angle, int speed ){
+  // moves the claw to a given open position based on angle provided,
+  // it returns the angle the claw has moved to when it has stopped moving
+  if(DEBUG) { std::cout << "Request angle: " << angle << " speed: " << speed << "\n";}
+  if(DEBUG) { std::cout << "Gear Ratio: " << clawGearRatio << " New angle: " << angle * clawGearRatio << "\n"; }
+
+  angle = clawGearRatio * angle;            // account for gear Ratio
+
+  // get current angle of the lift.
+  float currentAngle = clawMotor.get_position();
+
+  if(angle < 0) {
+    if(DEBUG) { std::cout << "Can not move down past ZERO position \n"; }
+  } else {
+    if(angle > (maxAngleClawOpen * clawGearRatio) ) {
+      // We need to accomedate gear ratio in calculation the true angle is adjusted
+      // by liftGearRatio for the actual motor move angle.
+      if(DEBUG) { std::cout << "Can not move past maximum angle of:" << maxAngleClawOpen << " \n"; }
+    } else {
+      // we can move so lets....
+      // calculate the target window
+      float minAngle = angle - 5;
+      float maxAngle = angle + 5;
+      if(DEBUG) { std::cout << "angle: " << angle << " minAngle: " << minAngle << " maxAngle: " << maxAngle << "\n";}
+      liftMotor.move_absolute(angle, speed);
+      while (!((clawMotor.get_position() < maxAngle) && (clawMotor.get_position() > minAngle))) {
+        pros::delay(2);
+      }
+    }
+  }
+  if(DEBUG) { std::cout << "Final angle: " << clawMotor.get_position() << " True angle: " << (liftMotor.get_position() / clawGearRatio) << " \n"; }
+  return(liftMotor.get_position());
 }
